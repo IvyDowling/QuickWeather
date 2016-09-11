7fb638632e51682f155e4c8312d94e510c47e8bf
MichaelIV <michaeldowlingiv@gmail.com>
2016-09-11 16:18:44 -0400

    Python one is so short, it's great

--- QuickWeather.py
+++ QuickWeather.py
@@ -3,9 +3,8 @@ import sys
 
 if len(sys.argv) is 0:
     print("try: QuickWeather [city]")
-else :
-    WEATHER_URL = "api.openweathermap.org/data/2.5/weather?q=" + sys.argv[1:] + " &APPID=b280819425992bbd289902c6fbf2ce55"
-    print("Searching for weather in " + sys.argv[1:] + " @ \n" + WEATHER_URL)
+else:
+    WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + sys.argv[1] + "&APPID=b280819425992bbd289902c6fbf2ce55"
+    print("Searching for weather in " + sys.argv[1] + " @ \n" + WEATHER_URL)
     json = urllib.request.urlopen(WEATHER_URL).read()
-    urllib.close
     print(json)
