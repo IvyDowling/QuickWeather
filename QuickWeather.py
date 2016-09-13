f9c74f53b464e8fc1fa8b0d0724b26bc935f692f
MichaelIV <michaeldowlingiv@gmail.com>
2016-09-13 13:29:27 -0400

    python now uses json obj to store api response

--- QuickWeather.py
+++ QuickWeather.py
@@ -1,10 +1,17 @@
-import urllib.request
+import requests
 import sys
+import json
 
-if len(sys.argv) is 0:
-    print("try: QuickWeather [city]")
-else:
-    WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + sys.argv[1] + "&APPID=b280819425992bbd289902c6fbf2ce55"
-    print("Searching for weather in " + sys.argv[1] + " @ \n" + WEATHER_URL)
-    json = urllib.request.urlopen(WEATHER_URL).read()
-    print(json)
+def getWeather():
+    if len(sys.argv) < 2:
+        print("try: QuickWeather [city]")
+    else:
+        WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + sys.argv[1] + "&APPID=b280819425992bbd289902c6fbf2ce55"
+        print("Searching for weather in " + sys.argv[1] + " @ \n" + WEATHER_URL)
+        api_response = requests.get(WEATHER_URL).json()
+        print(api_response)
+
+
+if __name__ == '__main__':
+    getWeather()
+
